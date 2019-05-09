fa681f7d3352621b8bcc107e443f03ccc964bec6
Natalie J <natsetc@gmail.com>
2019-05-09 03:41:45 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length >= 3
+    array.last.sub!(array[-1],"and #{array[-1]}")
+    array.join(", ")
+  elsif array.length == 2
+    array.insert(-2, " and ")
+    array.join
+  else array.length == 1
+    array.join
+  end
 end
